6d22ce0daed7112ec9fef78527e0e9768f8c2487
anushabc <66515706+anushabc@users.noreply.github.com>
2020-11-30 14:53:40 +0530

    Update logontask.ps1

--- artifacts/environment-setup/spektra/logontask.ps1
+++ artifacts/environment-setup/spektra/logontask.ps1
@@ -1,5 +1,5 @@
 Start-Transcript -Path C:\WindowsAzure\Logs\logontasklogs.txt -Append
-cd 'C:\LabFiles\synapse-ws-L400\artifacts\environment-setup\automation'
+cd 'C:\LabFiles\synapse-ws-L300\artifacts\environment-setup\automation'
 
 ./01-environment-setup.ps1
 ./03-environment-validate.ps1
